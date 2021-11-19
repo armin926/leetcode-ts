3c4b5a0f07388769f57d15b4f7dbb647a81e883f
armin <122699029@qq.com>
2021-11-19 11:36:17 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,51 @@
+
+## :pencil2: 前端力扣刷题
+不积跬步无以至千里，许多人在学习算法的路上放弃，第一是因为起点太高，刚上来就做一些中等难度甚至困难的题，遇到挫折就很难坚持下去，就算刚开始从简单做起，也会在基础尚未牢固的情况下在中等难度下退缩；第二是因为没有路线图，在没有刷题规划的时候，就只能随缘打开任意一题开始了，同样很难坚持下来。本仓库通过剑指Offer学习计划，每日更新自学的算法题，系统的学习算法与数据结构。
+
+## :tada: 题目大纲
+
+<table>
+    <tr>
+        <th>题目编号</th>
+        <th>题目名</th>
+        <th>难度</th>
+    </tr>
+    <tr>
+        <td>剑指Offer 09</td>
+        <td>
+        <a href="/剑指Offer/Day-1/剑指 Offer 09. 用两个栈实现队列.ts">用两个栈实现队列</a>
+        </td>
+        <td>简单</td>
+    </tr>
+    
+</table>
+
+<style>
+table {
+    width: 100%; /*表格宽度*/
+    margin: 15px auto; /*外边距*/
+    border-collapse: collapse; /*使用单一线条的边框*/
+    empty-cells: show; /*单元格无内容依旧绘制边框*/
+}
+
+table th,
+table td {
+  width: 33%;
+  height: 35px; /*统一每一行的默认高度*/
+  border: 1px solid #dedede; /*内部边框样式*/
+  padding: 0 10px; /*内边距*/
+  text-align: center;
+}
+table th {
+    font-weight: bold; /*加粗*/
+    text-align: center !important; /*内容居中，加上 !important 避免被 Markdown 样式覆盖*/
+    background: rgba(158,188,226,0.2); /*背景色*/
+    white-space: nowrap;
+}
+table tbody tr:nth-child(2n) {
+    background: rgba(158,188,226,0.12); 
+}
+table tr:hover {
+    background: #efefef;
+}
+</style>
---
+++ 剑指Offer/Day-1/剑指 Offer 09. 用两个栈实现队列.ts
@@ -0,0 +1,44 @@
+/*
+ * @Descripttion: 剑指 Offer 09. 用两个栈实现队列
+ * @Author: armin
+ * @Date: 2021-11-19 10:26:39
+ * @LastEditors: armin
+ * @LastEditTime: 2021-11-19 10:40:57
+ */
+/*
+ * 题目名称： 剑指 Offer 09. 用两个栈实现队列
+
+ * 题目描述： 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ *           分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ * 
+ * 问题分析： 栈是先进后出，队列是先进先出，我们用两个栈实现，将一个栈当作输入栈，另一个栈当作输出栈。当删除元素时，判断输出栈是否有值，
+ *           若无值，将输入栈依次出栈放入输出栈中，这样输出栈顶元素就是输入栈中先进入的元素，然后将输出栈顶弹出即可。
+ * 解题思路： 1、声明两个栈
+ *           2、appendTail操作，向输入栈中添加元素
+ *           3、删除操作时，我们不需要每次都将输入栈元素放入输出栈，只需要对输出栈进行判断，若输出栈中有值，则说明已经进行过移入操作，
+ *              我们可以直接将输出栈栈中元素移除栈顶即可。如果输出栈为空，再将输入栈元素依次放入输出栈，再移除输出栈的栈顶元素
+*/
+
+class CQueue {
+  protected inStack:number[] = [] // 初始化输入栈
+  protected outStack:number[] = [] // 初始化输出栈
+
+  appendTail(value: number):void {
+    // 输入栈尾部插入元素
+    this.inStack.push(value)
+  }
+
+  deleteHead():number {
+    // 判断输出栈是否为空
+    if(!this.outStack.length) {
+      // 判断输入栈是否为空
+      while(this.inStack.length) {
+        // 输出栈将输入栈元素依次放入
+        this.outStack.push(this.inStack.pop())
+      }
+    }
+    // 经过上述操作后若输出栈有值则返回移除的栈顶元素，若没有值则返回 -1
+    // PS： ?? 操作符： 若左边元素为null或undefined则直接返回右边元素，若左边元素有值则直接返回左边元素
+    return this.outStack.pop() ?? -1
+  }
+}
